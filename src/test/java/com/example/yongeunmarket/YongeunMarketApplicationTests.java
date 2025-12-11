3bea39e077c4f033607113da00a52c2bebe002c5
Juwoojae <juwqq1234@naver.com>
2025-12-11 11:32:27 +0900

    first commit

---
+++ src/test/java/com/example/yongeunmarket/YongeunMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yongeunmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongeunMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
