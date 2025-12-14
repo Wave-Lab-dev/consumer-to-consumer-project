4a0d96bf8e0ab073575de7d7b4cfc592d372fbec
Juwoojae <juwqq1234@naver.com>
2025-12-14 23:51:00 +0900

    feat: 상품 수정 API 구현 (#6) - ProductController 에 상품 수정 api 구헌 - 상품 수정 요청, 응답 Dto 생성 및 구현 - ProductService 에 상품 수정 비즈니스 로직 구현 - Product Entity 에 update 메서드 추가

--- src/main/java/com/example/yongeunmarket/controller/ProductController.java
+++ src/main/java/com/example/yongeunmarket/controller/ProductController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.yongeunmarket.dto.product.UpdateProductReqDto;
+import com.example.yongeunmarket.dto.product.UpdateProductResDto;
 import com.example.yongeunmarket.dto.product.GetProductResDto;
 import com.example.yongeunmarket.dto.product.CreateProductReqDto;
 import com.example.yongeunmarket.dto.product.CreateProductResDto;
@@ -42,11 +45,24 @@ public class ProductController {
 		List<GetProductResDto> getProductResDtos = productService.findAll();
 		return new ResponseEntity<>(getProductResDtos, HttpStatus.OK);
 	}
+
 	@GetMapping("/{productId}")
 	public ResponseEntity<GetProductResDto> getProduct(@PathVariable Long productId) {
 
 		GetProductResDto getProductResDto = productService.findById(productId);
 		return new ResponseEntity<>(getProductResDto, HttpStatus.OK);
 	}
+
+	@PatchMapping("/{productId}")
+	public ResponseEntity<UpdateProductResDto> modifyProduct(
+		@RequestBody @Valid UpdateProductReqDto updateProductReqDto,
+		@PathVariable Long productId) {
+		//@AuthenticationPrincipal CustomUserDetails
+		// Long userId = CustomUserDetails.getUsername();
+		//인증인가 미 구현으로 임시 하드코딩
+		UpdateProductResDto updateProductResDto = productService.updateProduct(updateProductReqDto, productId, 1L);
+		return new ResponseEntity<>(updateProductResDto, HttpStatus.OK);
+	}
+
 }
 
--- src/main/java/com/example/yongeunmarket/service/ProductService.java
+++ src/main/java/com/example/yongeunmarket/service/ProductService.java
@@ -4,17 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.yongeunmarket.dto.product.CreateProductReqDto;
 import com.example.yongeunmarket.dto.product.CreateProductResDto;
 import com.example.yongeunmarket.dto.product.GetProductResDto;
+import com.example.yongeunmarket.dto.product.UpdateProductReqDto;
+import com.example.yongeunmarket.dto.product.UpdateProductResDto;
 import com.example.yongeunmarket.entity.Product;
 import com.example.yongeunmarket.entity.User;
 import com.example.yongeunmarket.repository.ProductRepository;
 import com.example.yongeunmarket.repository.UserRepository;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -47,6 +49,7 @@ public class ProductService {
 			.build();
 	}
 
+	@Transactional(readOnly = true)
 	public List<GetProductResDto> findAll() {
 
 		List<GetProductResDto> results = new ArrayList<>();
@@ -63,6 +66,7 @@ public class ProductService {
 		return results;
 	}
 
+	@Transactional(readOnly = true)
 	public GetProductResDto findById(Long productId) {
 
 		Product product = getProductOrThrow(productId);
@@ -73,6 +77,32 @@ public class ProductService {
 			.price(product.getPrice())
 			.description(product.getDescription())
 			.createdAt(product.getCreatedAt())
+			.build();
+	}
+
+	@Transactional
+	public UpdateProductResDto updateProduct(
+		UpdateProductReqDto updateProductReqDto, Long productId, Long userId) {
+
+		Product product = getProductOrThrow(productId);
+		// 상품이 현재 로그인한 user 가 만든 product 인지 검증
+		if (!userId.equals(product.getUser().getId())) {
+			throw new IllegalStateException("해당 user 에 권한이 없습니다");    //403
+		}
+
+		product.updateProduct(
+			updateProductReqDto.getName(),
+			updateProductReqDto.getPrice(),
+			updateProductReqDto.getDescription());
+
+		return UpdateProductResDto.builder()
+			.productId(product.getId())
+			.userId(userId)
+			.name(product.getName())
+			.price(product.getPrice())
+			.description(product.getDescription())
+			.createdAt(product.getCreatedAt())
+			.updatedAt(product.getUpdatedAt())
 			.build();
 	}
 
