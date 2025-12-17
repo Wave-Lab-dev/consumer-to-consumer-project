172524756a3a3203613ee6176f7be33b05b7db98
heo <foresty110@gmail.com>
2025-12-17 19:37:30 +0900

    update: authenticationManager bean등록 및 userDetailsService연결 (#2)

--- src/main/java/com/example/yongeunmarket/config/SecurityConfig.java
+++ src/main/java/com/example/yongeunmarket/config/SecurityConfig.java
@@ -1,12 +1,18 @@
 package com.example.yongeunmarket.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +29,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
 	private final JwtFilter jwtFilter;
+	private final UserDetailsService userDetailsService;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -45,5 +52,14 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager() {
+		DaoAuthenticationProvider userAuthProvider = new DaoAuthenticationProvider();
+		userAuthProvider.setUserDetailsService(userDetailsService);
+		userAuthProvider.setPasswordEncoder(passwordEncoder());
+		
+		return new ProviderManager(Arrays.asList(userAuthProvider));
 	}
 }
