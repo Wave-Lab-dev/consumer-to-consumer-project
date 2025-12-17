e09973fe0fe80bdbd206959d70018cacda6d68de
Juwoojae <juwqq1234@naver.com>
2025-12-17 21:48:29 +0900

    feat: 비밀번호 재설정 API Controller 구현(#14) - 재설정 코드를 검증후 새로운 비밀번호로 바꾸는 API 구현

--- src/main/java/com/example/yongeunmarket/controller/PasswordResetController.java
+++ src/main/java/com/example/yongeunmarket/controller/PasswordResetController.java
@@ -3,9 +3,11 @@ package com.example.yongeunmarket.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.yongeunmarket.dto.user.VerifyPasswordReqDto;
 import com.example.yongeunmarket.service.PasswordResetService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,15 @@ public class PasswordResetController {
 		//@AuthenticationPrincipal CustomUserDetails
 		Long userId = 1L; // userDetails.getUsername() 인증 인가 미구현으로 인한 하드코딩
 		passwordResetService.sendResetCode(userId);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PostMapping("/reset-password")
+	public ResponseEntity<Void> resetPassword(
+		@RequestBody VerifyPasswordReqDto verifyPasswordReqDto) {
+		//@AuthenticationPrincipal CustomUserDetails
+		Long userId = 1L; // userDetails.getUsername() 인증 인가 미구현으로 인한 하드코딩
+		passwordResetService.resetPassword(verifyPasswordReqDto, userId);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
