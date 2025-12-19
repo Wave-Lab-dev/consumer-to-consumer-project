33b2656adf74fa56c2dd56afb5b12b798c791984
Juwoojae <juwqq1234@naver.com>
2025-12-19 12:05:51 +0900

    refactor: 피드백 반영 SDK1 -> SDK2 로 마이그레이션 (#17) - configs 패키지를 config 로 통일 - multipart 를 InputStream 으로 받아서 동기로전달하는 방식은 유지 한채로   마이그레이션 - 피드백에서 언급되었던 내용 리펙토링   - png, jpg, jpeg 같은 확장자 검증   - S3 의 보상 트랜잭션 처리   - 삭제시 로그 남기기

--- src/main/java/com/example/yongeunmarket/service/S3UploadService.java
+++ src/main/java/com/example/yongeunmarket/service/S3UploadService.java
@@ -1,7 +1,7 @@
 package com.example.yongeunmarket.service;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -9,26 +9,29 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.example.yongeunmarket.entity.User;
 import com.example.yongeunmarket.repository.UserRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+@Slf4j(topic = "S3UploadService")
 @Service
 @RequiredArgsConstructor
 public class S3UploadService {
 
-	/**
-	 * AmazonS3 는 AmazonS3Client의 인터페이스 이므로
-	 * 등록된 빈(AmazonS3Client의)이 자동 매핑된다.
-	 */
-	private final AmazonS3 amazonS3;
+	private final S3Client s3Client;
 
 	private final UserRepository userRepository;
+
+	private static final Set<String> ALLOWED_EXTENSIONS = Set.of("jpg", "jpeg", "png");
+	private static final String IMAGES_PREFIX = "images/";
 
 	@Value("${spring.cloud.aws.s3.bucket}")
 	private String bucket;
@@ -47,52 +50,66 @@ public class S3UploadService {
 		}
 
 		if (StringUtils.hasText(user.getImageUrl())) {
-			deleteImage(user.getImageUrl());
+			deleteObject(user.getImageUrl());
 		}
 
 		String originalFilename = multipartFile.getOriginalFilename();
 
 		String extension = getExtension(originalFilename);
-		String key = "images/" + UUID.randomUUID() + "." + extension;
+		String key = IMAGES_PREFIX + UUID.randomUUID() + "." + extension;
 
-		ObjectMetadata metadata = new ObjectMetadata();
-		metadata.setContentLength(multipartFile.getSize());
-		metadata.setContentType(multipartFile.getContentType());
-
+		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+			.bucket(bucket)
+			.key(key)
+			.contentType(multipartFile.getContentType())
+			.build();
 		try {
-			InputStream inputStream = multipartFile.getInputStream();
-			amazonS3.putObject(bucket, key, inputStream, metadata);
+			s3Client.putObject(
+				putObjectRequest,
+				RequestBody.fromInputStream(multipartFile.getInputStream(), multipartFile.getSize())
+			);
 			user.updateImageUrl(key);    //서버가 bucket 을 저장하고 있으으로, pull path 가 아닌 key 를 저장한다.
-		} catch (IOException e) {
+		} catch (IOException ex) {
+			log.error("InputStream 실패 오류 {}", ex.getMessage());
 			throw new IllegalStateException("multipart file upload failed !!");
+		} catch (Exception ex) {
+			deleteObject(key);  //보상 트랜잭션 처리
+			log.error("DB 트랜잭션 실패 오류 {}", ex.getMessage());
+			throw new IllegalStateException("database error !!");
 		}
 	}
 
-	/**
-	 * s3 에서 image file 삭제 하기
-	 * @param imageUrl
-	 */
-	public void deleteImage(String imageUrl) {
+	public void deleteObject(String key) {
 
-		amazonS3.deleteObject(bucket, imageUrl);
+		DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+			.bucket(bucket)
+			.key(key)
+			.build();
+		s3Client.deleteObject(deleteObjectRequest);  // 동기 삭제 (blocking)
 	}
 
 	/**
 	 * 확장자를 가지고 오는 헬퍼 메서드
+	 * 확장자가 png, jpg, jpeg 가 아닌경우 예외를 던진다.
 	 * @param filename : 가지고 오는 originalFilename
 	 * @return : 파일 확장자 (예: jpg, png)
 	 * @throws IllegalArgumentException 확장자가 없는 경우
 	 */
 	private String getExtension(String filename) {
+
 		int lastDotIndex = filename.lastIndexOf('.');
 		if (lastDotIndex == -1) {
 			throw new IllegalArgumentException("Invalid file extension");
 		}
-		return filename.substring(lastDotIndex + 1);
-	}
+		String extension = filename.substring(lastDotIndex + 1);
+		if (!ALLOWED_EXTENSIONS.contains(extension)) // 확장자가 jpg, jpeg, png 가 아닌 경우
+			throw new IllegalArgumentException("Invalid file extension");
+		return extension;
+}
 
-	private User getUserOrThrow(Long userId) {
-		return userRepository.findById(userId).orElseThrow(
-			() -> new EntityNotFoundException("user 가 존재하지 않음"));
-	}
+private User getUserOrThrow(Long userId) {
+
+	return userRepository.findById(userId).orElseThrow(
+		() -> new EntityNotFoundException("user 가 존재하지 않음"));
+}
 }
