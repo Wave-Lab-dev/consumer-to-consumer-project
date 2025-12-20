a0c5c445bd22f14ceca735cf9846d08c457e6de9
Juwoojae <juwqq1234@naver.com>
2025-12-20 17:08:37 +0900

    refactor: Controller 리펙토링 (#37) - ProductController, S3UploadController 줄바꿈 적용 - PasswordResetController 인증, 인가 설정 추가

--- src/main/java/com/example/yongeunmarket/controller/PasswordResetController.java
+++ src/main/java/com/example/yongeunmarket/controller/PasswordResetController.java
@@ -2,12 +2,14 @@ package com.example.yongeunmarket.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.yongeunmarket.dto.user.VerifyPasswordReqDto;
+import com.example.yongeunmarket.security.CustomUserDetails;
 import com.example.yongeunmarket.service.PasswordResetService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,18 +22,20 @@ public class PasswordResetController {
 	private final PasswordResetService passwordResetService;
 
 	@PostMapping("/reset-code")
-	public ResponseEntity<Void> sendResetCode() {
-		//@AuthenticationPrincipal CustomUserDetails
-		Long userId = 1L; // userDetails.getUsername() 인증 인가 미구현으로 인한 하드코딩
+	public ResponseEntity<Void> sendResetCode(
+		@AuthenticationPrincipal CustomUserDetails customUserDetails
+	) {
+		Long userId = customUserDetails.getUserId();
 		passwordResetService.sendResetCode(userId);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 	@PostMapping("/reset-password")
 	public ResponseEntity<Void> resetPassword(
-		@RequestBody VerifyPasswordReqDto verifyPasswordReqDto) {
-		//@AuthenticationPrincipal CustomUserDetails
-		Long userId = 1L; // userDetails.getUsername() 인증 인가 미구현으로 인한 하드코딩
+		@RequestBody VerifyPasswordReqDto verifyPasswordReqDto,
+		@AuthenticationPrincipal CustomUserDetails customUserDetails
+	) {
+		Long userId = customUserDetails.getUserId();
 		passwordResetService.resetPassword(verifyPasswordReqDto, userId);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
