c273304be831bc34e3c7367cf26f86e0388e9a8b
Juwoojae <juwqq1234@naver.com>
2025-12-23 12:22:51 +0900

    fixup! refactor: 커스텀 예외 추가 (#37) - 커스텀 예외 추가후, 수정

---
+++ src/main/java/com/example/yongeunmarket/exception/TooManyAttemptsException.java
@@ -0,0 +1,10 @@
+package com.example.yongeunmarket.exception;
+/**
+ * 재설정 코드 입력 시도 횟수 초과
+ */
+public class TooManyAttemptsException extends RuntimeException {
+
+	public TooManyAttemptsException(String message) {
+		super(message);
+	}
+}
